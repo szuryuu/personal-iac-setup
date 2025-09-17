cdd4b06a3ed7e6e45f6fbaa2e2afdb8a2529de22
shfwnz <ilhamdzaky2007@gmail.com>
2025-09-17 14:41:48 +0700

    fix: terratest conf

--- terraform/environments/staging/test/main_test.go
+++ terraform/environments/staging/test/main_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 
@@ -10,12 +11,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func getRequiredEnvVar(t *testing.T, envVar string) string {
+	value := os.Getenv(envVar)
+	if value == "" {
+		t.Fatalf("Environment variable %s is required but not set", envVar)
+	}
+	return value
+}
+
+func TestTerraformPlan(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "..",
+	}
+
+	terraform.Init(t, terraformOptions)
+	planOutput := terraform.Plan(t, terraformOptions)
+
+	assert.Contains(t, planOutput, "azurerm_linux_virtual_machine")
+	assert.Contains(t, planOutput, "azurerm_mysql_flexible_server")
+	assert.Contains(t, planOutput, "azurerm_virtual_network")
+}
+
 func TestTerraformAzureInfrastructure(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping integration test in short mode")
 	}
 
-	t.Parallel()
+	// t.Parallel()
 
 	uniqueID := random.UniqueId()
 	projectName := fmt.Sprintf("test-%s", strings.ToLower(uniqueID))
@@ -24,6 +46,11 @@ func TestTerraformAzureInfrastructure(t *testing.T) {
 		TerraformDir: "..",
 		Vars: map[string]any{
 			"project_name": projectName,
+		},
+		EnvVars: map[string]string{
+			"TF_VAR_subscription_id":     getRequiredEnvVar(t, "TF_VAR_subscription_id"),
+			"TF_VAR_resource_group_name": getRequiredEnvVar(t, "TF_VAR_resource_group_name"),
+			"TF_VAR_key_vault_name":      getRequiredEnvVar(t, "TF_VAR_key_vault_name"),
 		},
 	})
 
@@ -39,16 +66,4 @@ func TestTerraformAzureInfrastructure(t *testing.T) {
 	assert.NotEmpty(t, vmPrivateIP)
 	assert.NotEmpty(t, dbFQDN)
 	assert.Contains(t, dbFQDN, "mysql.database.azure.com")
-}
-
-func TestTerraformPlan(t *testing.T) {
-	terraformOptions := &terraform.Options{
-		TerraformDir: "..",
-	}
-
-	terraform.Init(t, terraformOptions)
-	planOutput := terraform.Plan(t, terraformOptions)
-
-	assert.Contains(t, planOutput, "azurerm_linux_virtual_machine")
-	assert.Contains(t, planOutput, "azurerm_mysql_flexible_server")
 }
