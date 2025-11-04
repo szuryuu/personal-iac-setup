0b186ea677b19e343adc36f6550ddf476afd904c
shfwnz <ilhamdzaky2007@gmail.com>
2025-11-04 11:10:08 +0700

    fix: proxysql

--- terraform/modules/compute/scripts/db-init.sh
+++ terraform/modules/compute/scripts/db-init.sh
@@ -4,11 +4,20 @@ set -euo pipefail
 exec > >(tee /var/log/db-install.log)
 exec 2>&1
 
-echo "Update"
+# Update
 sudo apt-get update -y
 
-echo "Install MySQL Server"
-sudo DEBIAN_FRONTEND=noninteractive apt install -y mysql-server
+# ProxySQL Key
+apt-get install -y --no-install-recommends lsb-release wget apt-transport-https ca-certificates gnupg
+wget -O - 'https://repo.proxysql.com/ProxySQL/proxysql-3.0.x/repo_pub_key' | apt-key add -
+echo deb https://repo.proxysql.com/ProxySQL/proxysql-3.0.x/$(lsb_release -sc)/ ./ | tee /etc/apt/sources.list.d/proxysql.list
+
+# Update
+# TERM=xterm sudo nano /etc/proxysql.cnf
+sudo apt-get update -y
+
+echo "Installing some tools"
+sudo DEBIAN_FRONTEND=noninteractive apt install -y mysql-server proxysql
 
 sudo systemctl enable mysql
 sudo systemctl start mysql
@@ -20,11 +29,27 @@ echo "Set root password and create database"
 mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${db_password}';
 FLUSH PRIVILEGES;
+
 CREATE DATABASE IF NOT EXISTS ${db_name};
 CREATE USER IF NOT EXISTS '${db_username}'@'%' IDENTIFIED BY '${db_password}';
 GRANT ALL PRIVILEGES ON *.* TO '${db_username}'@'%' WITH GRANT OPTION;
+
+CREATE USER IF NOT EXISTS '${db_username}'@'127.0.0.1' IDENTIFIED BY '${db_password}';
+GRANT ALL PRIVILEGES ON *.* TO '${db_username}'@'127.0.0.1' WITH GRANT OPTION;
+
+CREATE USER IF NOT EXISTS 'monitor'@'%' IDENTIFIED BY 'monitor';
+GRANT USAGE, REPLICATION CLIENT ON *.* TO 'monitor'@'%';
+
 FLUSH PRIVILEGES;
 EOF
 
 echo "Restart MySQL"
 sudo systemctl restart mysql
+
+echo "ProxySQL Setup"
+sudo systemctl start proxysql
+sudo systemctl enable proxysql
+sudo systemctl restart proxysql
+
+service proxysql initial
+proxysql --version
